bacf497fe7ffa929cf3bdcabbd6e2c46164c6f8d
kausar <43438240+kausar3033@users.noreply.github.com>
2023-10-23 16:53:31 +0600

    Create single2.sh

---
+++ single2.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -x
+sudo apt-get update
+apt install sshpass
+apt-get install -y curl openssh-server
+
+echo $(hostname -i) $(hostname) >> /etc/hosts
+sudo sed -i "/swap/s/^/#/" /etc/fstab
+sudo swapoff -a
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+apt-get install -y ebtables ethtool
+apt-get install -y docker.io
+
+cat <<EOF >/etc/docker/daemon.json
+{
+  "bip": "172.123.0.1/16"
+}
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
+
+apt-get install -y apt-transport-https
+apt-get install -qy kubelet=1.28.2-00 kubeadm=1.28.2-00 kubectl=1.28.2-00
+
+chmod 755 token.sh
+sh token.sh
