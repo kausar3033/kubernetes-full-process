0a327f5c6319debd36d41721dd5e9f7ebf392d85
kausar <43438240+kausar3033@users.noreply.github.com>
2023-10-24 15:27:23 +0600

    Update single.sh

--- single.sh
+++ single.sh
@@ -8,77 +8,6 @@ echo $(hostname -i) $(hostname) >> /etc/hosts
 sudo sed -i "/swap/s/^/#/" /etc/fstab
 sudo swapoff -a
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-apt-get update
-
-
-apt-get install -y ebtables ethtool
-apt-get install -y docker.io
-
-cat <<EOF >/etc/docker/daemon.json
-{
-  "bip": "172.123.0.1/16"
-}
-EOF
-
-systemctl daemon-reload
-systemctl restart docker
-
-apt-get install -y apt-transport-https
-apt-get install -qy kubelet=1.25.3-00 kubeadm=1.25.3-00 kubectl=1.25.3-00
-
-apt-mark hold docker.io kubelet kubeadm kubectl
-
-sudo systemctl enable docker
-sudo systemctl enable kubelet
-
-. /etc/os-release
-if [ "$UBUNTU_CODENAME" = "bionic" ]; then
-    modprobe br_netfilter
-fi
-sysctl net.bridge.bridge-nf-call-iptables=1
-
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
-kubeadm token create --print-join-command >>token.sh
-
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/metallb.yaml
-echo -e "apiVersion: v1\nkind: ConfigMap\nmetadata:\n  namespace: metallb-system\n  name: config\ndata:\n  config: |\n    address-pools:\n    - name: default\n      protocol: layer2\n      addresses:\n      - 10.209.99.107-10.209.99.108" >> metallb-configmap.yaml
-chmod +x metallb-configmap.yaml
-kubectl apply -f metallb-configmap.yaml
-
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
-
-helm -n staging repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm -n staging repo add  stable https://charts.helm.sh/stable	
-helm repo add nginx-stable https://helm.nginx.com/stable
-helm -n staging  repo update
-helm -n staging  repo list
-
-kubectl get ns
-kubectl create namespace staging
-kubectl run nginx --image=nginx --namespace=staging
-kubectl config set-context --current --namespace=staging
-helm -n staging install ibosio-ingress nginx-stable/nginx-ingress --set controller.service.type=LoadBalancer,controller.ingressClass=ibosio-ingress
-kubectl -n staging get all 
-kubectl get svc
-kubectl get pods
-
 echo -e "-----BEGIN CERTIFICATE-----
 MIIGEzCCA/ugAwIBAgIQfVtRJrR2uhHbdBYLvFMNpzANBgkqhkiG9w0BAQwFADCB
 iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl
@@ -201,7 +130,80 @@ mQBrQV8difAYM+yRddIEC2kIjB85ztbOL+freDZqcFZiG4MKjL27L+hI9cNaKGVa
 gxP+c2C9UlgdIA7VdNsPT8a0sHEuQlp25CMwoOnWRKfE/3V7DshHzHhXC3SQQ1FK
 YeRNs/BhYYgB/SjeTIOdzA==
 -----END PRIVATE KEY-----" >> key.pem
-chmod 444 key.pem cert.pem
+chmod 644 key.pem cert.pem
+
+echo -e "apiVersion: v1\nkind: ConfigMap\nmetadata:\n  namespace: metallb-system\n  name: config\ndata:\n  config: |\n    address-pools:\n    - name: default\n      protocol: layer2\n      addresses:\n      - 10.209.99.107-10.209.99.108" >> metallb-configmap.yaml
+chmod +x metallb-configmap.yaml
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+
+
+apt-get install -y ebtables ethtool
+apt-get install -y docker.io
+
+cat <<EOF >/etc/docker/daemon.json
+{
+  "bip": "172.123.0.1/16"
+}
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
+
+apt-get install -y apt-transport-https
+apt-get install -qy kubelet=1.25.3-00 kubeadm=1.25.3-00 kubectl=1.25.3-00
+
+apt-mark hold docker.io kubelet kubeadm kubectl
+
+sudo systemctl enable docker
+sudo systemctl enable kubelet
+
+. /etc/os-release
+if [ "$UBUNTU_CODENAME" = "bionic" ]; then
+    modprobe br_netfilter
+fi
+sysctl net.bridge.bridge-nf-call-iptables=1
+
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+kubeadm token create --print-join-command >>token.sh
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/metallb.yaml
+kubectl apply -f metallb-configmap.yaml
+
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+helm -n staging repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm -n staging repo add  stable https://charts.helm.sh/stable	
+helm repo add nginx-stable https://helm.nginx.com/stable
+helm -n staging  repo update
+helm -n staging  repo list
+
+kubectl get ns
+kubectl create namespace staging
+kubectl run nginx --image=nginx --namespace=staging
+kubectl config set-context --current --namespace=staging
+helm -n staging install ibosio-ingress nginx-stable/nginx-ingress --set controller.service.type=LoadBalancer,controller.ingressClass=ibosio-ingress
+kubectl -n staging get all 	
+kubectl get svc
+
+
 kubectl -n staging create secret tls ibosio-ingress-tls --key key.pem --cert cert.pem
 
 echo -e 'apiVersion: networking.k8s.io/v1\nkind: Ingress\nmetadata:\n  annotations:\n    nginx.ingress.kubernetes.io/ssl-redirect: "true"\n    nginx.ingress.kubernetes.io/proxy-read-timeout: "3600"\n    nginx.ingress.kubernetes.io/proxy-send-timeout: "3600"\n    nginx.ingress.kubernetes.io/proxy-body-size: "0"\n     \n  name: ing-ibosapp\nspec:\n  ingressClassName: ibosio-ingress\n  rules:\n    #madina.ibos.local\n    - host: test.ibos.io\n      http:\n        paths:\n          - pathType: Prefix\n            path: /\n            backend:\n              service:\n                name: front\n                port:\n                  number: 80\n          - pathType: Prefix\n            path: /identity\n            backend:\n              service:\n                name: identity\n                port:\n                  number: 80\n          - pathType: Prefix\n            path: /vat\n            backend:\n              service:\n                name: vatapi\n                port:\n                  number: 80\n\n\n\n  tls:\n    - hosts:\n        - test.ibos.io\n       \n\n      secretName: ibosio-ingress-tls' >> ibos_ingress.yaml
