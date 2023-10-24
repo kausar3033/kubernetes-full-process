5e4b6e946f9e1a1a10bd84f3b74527e86b34f94b
kausar <43438240+kausar3033@users.noreply.github.com>
2023-10-24 16:43:58 +0600

    Update single.sh

--- single.sh
+++ single.sh
@@ -1,4 +1,97 @@
 #!/bin/sh
+set -x
+
+apt install sshpass
+apt-get install -y curl openssh-server
+
+echo $(hostname -i) $(hostname) >> /etc/hosts
+sudo sed -i "/swap/s/^/#/" /etc/fstab
+sudo swapoff -a
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+
+apt-get install -y ebtables ethtool
+apt-get install -y docker.io
+
+cat <<EOF >/etc/docker/daemon.json
+{
+  "bip": "172.123.0.1/16"
+}
+EOF
+
+systemctl daemon-reload
+systemctl restart docker
+
+apt-get install -y apt-transport-https
+apt-get install -qy kubelet=1.25.3-00 kubeadm=1.25.3-00 kubectl=1.25.3-00
+
+apt-mark hold docker.io kubelet kubeadm kubectl
+
+sudo systemctl enable docker
+sudo systemctl enable kubelet
+
+. /etc/os-release
+if [ "$UBUNTU_CODENAME" = "bionic" ]; then
+    modprobe br_netfilter
+fi
+sysctl net.bridge.bridge-nf-call-iptables=1
+
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+kubeadm token create --print-join-command >>token.sh
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/metallb.yaml
+cat <<EOL >> metallb-configmap.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - 10.209.99.107-10.209.99.108
+EOL
+
+chmod +x metallb-configmap.yaml
+kubectl apply -f metallb-configmap.yaml
+
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+helm -n staging repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm -n staging repo add  stable https://charts.helm.sh/stable	
+helm repo add nginx-stable https://helm.nginx.com/stable
+helm -n staging  repo update
+helm -n staging  repo list
+
+kubectl get ns
+kubectl create namespace staging
+kubectl run nginx --image=nginx --namespace=staging
+kubectl config set-context --current --namespace=staging
+helm -n staging install ibosio-ingress nginx-stable/nginx-ingress --set controller.service.type=LoadBalancer,controller.ingressClass=ibosio-ingress
+kubectl -n staging get all 	
+kubectl get svc
+
+
 cat <<EOL >> cert.pem
 -----BEGIN CERTIFICATE-----
 MIIGEzCCA/ugAwIBAgIQfVtRJrR2uhHbdBYLvFMNpzANBgkqhkiG9w0BAQwFADCB
@@ -126,23 +219,7 @@ YeRNs/BhYYgB/SjeTIOdzA==
 EOL
 
 chmod 644 key.pem cert.pem
-
-cat <<EOL >> metallb-configmap.yaml
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  namespace: metallb-system
-  name: config
-data:
-  config: |
-    address-pools:
-    - name: default
-      protocol: layer2
-      addresses:
-      - 10.209.99.107-10.209.99.108
-EOL
-
-chmod +x metallb-configmap.yaml
+kubectl -n staging create secret tls ibosio-ingress-tls --key key.pem --cert cert.pem
 
 cat <<EOL >> ibos_ingress.yaml
 apiVersion: networking.k8s.io/v1
@@ -195,4 +272,22 @@ spec:
 EOL
 
 chmod +x ibos_ingress.yaml
+kubectl -n staging apply -f ibos_ingress.yaml
 
+kubectl -n staging create secret docker-registry dockercred --docker-server=https://index.docker.io --docker-username=iboslimitedbd --docker-password=iBOS@ltd21 --docker-email=iboslimitedbd@gmail.com
+kubectl get node
+kubectl get pods
+
+sshpass -p "ibos@123" sftp -o StrictHostKeyChecking=no ibos@10.209.99.110:/home/ibos/ <<< "put /home/ibos/token.sh"
+
+rm ~/.bash_history
+history -c
+sshpass -p "ibos@123" ssh -o StrictHostKeyChecking=no ibos@10.209.99.110 'echo "ibos@123" | sudo -S sh -c "git clone https://github.com/kausar3033/kubernetes-full-process.git && cd kubernetes-full-process/ && chmod +x single2.sh && sed -i '\''s/\r$//'\'' single2.sh && mv single2.sh /home/ibos && cd /home/ibos && ./single2.sh"'
+
+kubectl get node
+kubectl get pods
+rm ~/.bash_history
+history -c
+rm -rf kubernetes-full-process single.sh
+rm ~/.bash_history
+history -c
