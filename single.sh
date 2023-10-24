d2737cc0cbe2f2dfd0089827377ded0d72ca7ea7
kausar <43438240+kausar3033@users.noreply.github.com>
2023-10-24 12:05:34 +0600

    Update single.sh

--- single.sh
+++ single.sh
@@ -29,7 +29,7 @@ systemctl daemon-reload
 systemctl restart docker
 
 apt-get install -y apt-transport-https
-apt-get install -qy kubelet==1.25.3-00 kubeadm==1.25.3-00 kubectl==1.25.3-00
+apt-get install -qy kubelet=1.25.3-00 kubeadm=1.25.3-00 kubectl=1.25.3-00
 
 apt-mark hold docker.io kubelet kubeadm kubectl
 
