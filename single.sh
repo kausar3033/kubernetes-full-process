ba9eaa01ad80dc4f925ebb6b4150a903b984318c
kausar <43438240+kausar3033@users.noreply.github.com>
2023-10-24 13:07:00 +0600

    Update single.sh

--- single.sh
+++ single.sh
@@ -212,12 +212,16 @@ kubectl -n staging create secret docker-registry dockercred --docker-server=http
 kubectl get node
 kubectl get pods
 
-rm -rf kubernetes-full-process
+sshpass -p "ibos@123" sftp -o StrictHostKeyChecking=no ibos@10.209.99.110:/home/ibos/ <<< "put /home/ibos/token.sh"
+
 rm ~/.bash_history
 history -c
 sshpass -p "ibos@123" ssh -o StrictHostKeyChecking=no ibos@10.209.99.110 'echo "ibos@123" | sudo -S sh -c "git clone https://github.com/kausar3033/kubernetes-full-process.git && cd kubernetes-full-process/ && chmod +x single2.sh && sed -i '\''s/\r$//'\'' single2.sh && mv single2.sh /home/ibos && cd /home/ibos && ./single2.sh"'
 
 kubectl get node
 kubectl get pods
+rm ~/.bash_history
+history -c
+rm -rf kubernetes-full-process single.sh
 rm ~/.bash_history
 history -c
