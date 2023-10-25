b3d8b135d352c9a3d87bf1b3727732577b549ded
kausar <43438240+kausar3033@users.noreply.github.com>
2023-10-25 12:18:55 +0600

    Update single.sh

--- single.sh
+++ single.sh
@@ -1,57 +1,6 @@
 #!/bin/sh -x
 set -x
-apt install sshpass
-apt-get install -y curl openssh-server
 
-export KUBECONFIG=/home/ibos/.kube/config
-
-echo $(hostname -i) $(hostname) >> /etc/hosts
-sudo sed -i "/swap/s/^/#/" /etc/fstab
-sudo swapoff -a
-
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-apt-get update
-
-
-apt-get install -y ebtables ethtool
-apt-get install -y docker.io
-
-cat <<EOF >/etc/docker/daemon.json
-{
-  "bip": "172.123.0.1/16"
-}
-EOF
-
-systemctl daemon-reload
-systemctl restart docker
-
-apt-get install -y apt-transport-https
-apt-get install -qy kubelet==1.25.3-00 kubeadm==1.25.3-00 kubectl==1.25.3-00
-
-apt-mark hold docker.io kubelet kubeadm kubectl
-
-sudo systemctl enable docker
-sudo systemctl enable kubelet
-
-. /etc/os-release
-if [ "$UBUNTU_CODENAME" = "bionic" ]; then
-    modprobe br_netfilter
-fi
-sysctl net.bridge.bridge-nf-call-iptables=1
-
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
-kubeadm token create --print-join-command >>token.sh
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/metallb.yaml
