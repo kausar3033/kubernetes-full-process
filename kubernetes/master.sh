e07788579862442ddbefa98e1a9eddc62f6df00f
kausar <43438240+kausar3033@users.noreply.github.com>
2023-10-25 11:53:19 +0600

    Update master.sh

--- kubernetes/master.sh
+++ kubernetes/master.sh
@@ -6,6 +6,10 @@ echo $(hostname -i) $(hostname) >> /etc/hosts
 sudo sed -i "/swap/s/^/#/" /etc/fstab
 sudo swapoff -a
 
+touch /etc/kubernetes/admin.conf
+touch /root/.kube/config
+
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
