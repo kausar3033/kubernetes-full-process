4c92d4f0fd1c99f54a6e71930be12089e85fd0d2
kausar <43438240+kausar3033@users.noreply.github.com>
2023-11-02 17:04:21 +0600

    Update master.sh

--- master.sh
+++ master.sh
@@ -48,3 +48,5 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
